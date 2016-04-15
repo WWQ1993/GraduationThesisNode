4e62435a03328643c3f0b343fd4644665f8912d9
WWQ1993 <516041710@qq.com>
2016-04-15 19:40:37 +0800

    optimization

--- handler/requestHandlers.js
+++ handler/requestHandlers.js
@@ -578,8 +578,8 @@ module.exports = {
                                 fireTypeId = results[0]['FireTypeId'];
                                 typeId = fireTypeId;
 
-                                tipStr += "\r\n" + "{" + inputArr[0]['detail'] + "}" + "*" + "[" + fireTypeInput + "]" + "→火灾类别" + "=> {" + inputArr[0]['detail'] + "}→"
-                                    + "(" + fireTypeInput + ")（" + (1.0 * f) + "," + (0.9 * c) + "）"
+                                tipStr +=  "{" + inputArr[0]['detail'] + "}" + "*" + "[" + fireTypeInput + "]" + "→火灾类别" + "=> {" + inputArr[0]['detail'] + "}→"
+                                    + "(" + fireTypeInput + ")（" + (1.0 * f) + "," + (0.9 * c) + "）</br>"
 
                                 f = (f * 1.0);
                                 c = (c * 0.9 * 1.0 * f);
@@ -769,8 +769,8 @@ module.exports = {
                                                                 var conclu = results[0]["DispatchId"],
                                                                     FireFighterNum = results[0]["FireFighterNum"],
                                                                     Equipment = results[0]["Equipment"];
-                                                                tipStr += '\n' + "{" + inputArr[0]['detail'] + "}DAO" +
-                                                                    "→[火灾类别 " + inputArr[1]['detail'] + "]+[火灾级别 " + fireLevelDes + "] =>" + "{" + inputArr[0]['detail'] + "}" + "→(出动人数:" + FireFighterNum + ")+(设备数目:" + Equipment + ")（" + (1.0 * f) + "," + (0.9 * c) + "）";
+                                                                tipStr +=   "{" + inputArr[0]['detail'] + "}DAO" +
+                                                                    "→[火灾类别 " + inputArr[1]['detail'] + "]+[火灾级别 " + fireLevelDes + "] =>" + "{" + inputArr[0]['detail'] + "}" + "→(出动人数:" + FireFighterNum + ")+(设备数目:" + Equipment + ")（" + (1.0 * f) + "," + (0.9 * c) + "）</br>";
 
                                                                 dispatchStr += "出动人数:" + FireFighterNum + "\n设备数目:" + Equipment +
                                                                     '\n' + "（Frequency=" + (1.0 * f) + ",Confidence=" + (0.9 * c) + "）";
@@ -787,6 +787,7 @@ module.exports = {
                                                             }
                                                             else {
                                                                 console.log('未找到派遣方案')
+                                                                dispatchStr='未找到派遣方案 火灾类别： '+typeId+'  火灾级别: '+LevelId;
                                                                 calb();
                                                             }
                                                         }
@@ -818,7 +819,7 @@ module.exports = {
                                                     f = results[ind]["Frequency"];
                                                     c = results[ind]["Confidence"];
 
-                                                    tipStr += '\n' + "{$事件}→([" + des1 + "])=> {$事件}→([" + des2 + "](" + f + "," + c + ")" /*"新习得知识："+des1+"+"+des2+"("+f+","+c+")"*/;
+                                                    tipStr +=  "{$事件}→([" + des1 + "])=> {$事件}→([" + des2 + "](" + f + "," + c + ")</br>" /*"新习得知识："+des1+"+"+des2+"("+f+","+c+")"*/;
                                                     calb();
                                                 };
                                                 func.ind = ind;
@@ -846,7 +847,7 @@ module.exports = {
                                                     f = results[ind]["Frequency"];
                                                     c = results[ind]["Confidence"];
 
-                                                    tipStr += '\n' + "{$事件}→([" + des1 + "])=> {$事件}→([" + des2 + "](" + f + "," + c + ")" /*"新习得知识："+des1+"+"+des2+"("+f+","+c+")"*/;
+                                                    tipStr +=   "{$事件}→([" + des1 + "])=> {$事件}→([" + des2 + "](" + f + "," + c + ")</br>" /*"新习得知识："+des1+"+"+des2+"("+f+","+c+")"*/;
                                                     calb();
                                                 };
                                                 func.ind = ind;
@@ -1166,235 +1167,5 @@ module.exports = {
                 cback();
             })
         }
-
-
-        //
-        //
-        //for (; i < inputArrLength; i++) {
-        //
-        //        var obj = inputArr[i],
-        //            title = obj.title.split('：')[0],
-        //            detail = obj.detail,
-        //            truth = obj.truth,
-        //            id = obj.id;
-        //        obj.truth1 = truth.split('(')[1].split(',')[0];
-        //        obj.truth0 = truth.split(')')[0].split(',')[1];
-        //
-        //
-        //        if ('t_firetype' !== id && Array.isArray(dbConfig.detail[id]) && detail) {
-        //            midCounter2++;
-        //            var dbStr = "select FireLevelid,FireLevelName from t_firelevel where " + dbConfig.detail[id][0] +
-        //                " IN (select " + dbConfig.detail[id][0] + "  from " + id + " where " +
-        //                dbConfig.detail[id][1] + "='" + detail + "')";
-        //            var objbind = {
-        //                input: obj,
-        //                selectCb: function (err, results) {
-        //                    if (err) {
-        //                        res.status(200).send({returnState: -1, returnMsg: err.message});
-        //                        console.log(err.message);
-        //                        return
-        //                    }
-        //                    console.log(this.input)
-        //
-        //                    if (results.length > 0) { //查询成功
-        //                        var arr = [];
-        //
-        //                        for (var i = 0; i < results.length; i++) {
-        //                            arr.push(results[i]);   //储存搜索结果
-        //                        }
-        //
-        //                        midResult.push({resultArr: arr, obj: this.input});
-        //
-        //                    }
-        //                    midCounter++;
-        //                    midHandler();
-        //                }
-        //            }
-        //            db.query(dbStr, objbind.selectCb.bind(objbind));
-        //        }
-        //    }
-        //
-        //
-        //    function midHandler() {
-        //        if (midCounter === midCounter2) {
-        //            console.log('\n');
-        //            console.log(midResult);
-        //
-        //            function outer(index) {
-        //                if (index >= midResult.length) {
-        //                    decideTypeLevel();
-        //                    return;
-        //                }
-        //                console.log('outer')
-        //
-        //                var obj = midResult[index],
-        //                    resultArr = obj['resultArr'],
-        //                    inputObj = obj['obj'];
-        //
-        //                function inner(ind) {
-        //                    if (ind >= resultArr.length) {
-        //                        outer(index + 1);
-        //                        return;
-        //                    }
-        //                    console.log('inner')
-        //
-        //                    var resultObj = resultArr[ind];
-        //                    var fireLevelId = resultObj['FireLevelid'];
-        //                    var fireLevelName = resultObj['FireLevelName'],
-        //                        f = inputObj.truth1,
-        //                        c = inputObj.truth0;
-        //                    //设置详细显示
-        //                    tipStr += '（$事件*[' + inputObj.detail + ']→' + inputObj.title + '=> $事件→([' +
-        //                        fireLevelName.split('火灾')[0] + '] &火灾)  (' + f + ',' + (0.9 * c) + ')</br>';
-        //
-        //
-        //                    // 插进中间结论表---------
-        //                    var sql = "select * from mConclusion where conclusion = " + fireLevelId;
-        //
-        //                    db.query(sql, function (err, results) {
-        //                        if (err) {
-        //                            res.status(200).send({returnState: -1, returnMsg: err.message});
-        //                            console.log(err.message);
-        //                            return
-        //                        }
-        //
-        //                        if (results.length > 0) { //查询成功
-        //                            var f2 = results[0]['f'],
-        //                                c2 = results[0]['c'],
-        //                                f1 = f * 1.0,
-        //                                c1 = ( c * 0.9 * 1.0 * f);
-        //
-        //                            console.log("f=" + f + " c=" + c + "   f1:" + f1 + " c1=" + c1);
-        //
-        //                            f = (f1 * c1 * (1 - c2) + f2 * c2 * (1 - c1)) / (c1 * (1 - c2) + c2 * (1 - c1));
-        //                            c = (c1 * (1 - c2) + c2 * (1 - c1)) / (c1 * (1 - c2) + c2 * (1 - c1) + (1 - c1) * (1 - c2));
-        //
-        //                            db.query("update mConclusion" + " set f=" + f +
-        //                                ",c=" + c + " where conclusion = " + fireLevelId, function (err, results) {
-        //                                if (err) {
-        //                                    res.status(200).send({returnState: -1, returnMsg: err.message});
-        //                                    console.log(err.message);
-        //                                    return
-        //                                }
-        //                                inner(ind + 1);
-        //                            });
-        //                        } else {
-        //                            db.query("insert into mConclusion(conclusion) values(" +
-        //                                fireLevelId + ")", function (err, results) {
-        //                                if (err) {
-        //                                    res.status(200).send({returnState: -1, returnMsg: err.message});
-        //                                    console.log(err.message);
-        //                                    return
-        //                                }
-        //
-        //                                f = f * 1.0;
-        //                                c = ( c * 0.9 * 1.0 * f);
-        //
-        //                                db.query("update mConclusion" + " set f=" + f + ",c=" +
-        //                                    c + " where conclusion = " + fireLevelId, function (err, results) {
-        //                                    if (err) {
-        //                                        res.status(200).send({returnState: -1, returnMsg: err.message});
-        //                                        console.log(err.message);
-        //                                        return
-        //                                    }
-        //                                    inner(ind + 1);
-        //                                });
-        //                            });
-        //                        }
-        //                    });
-        //                }
-        //
-        //                inner(0)
-        //            };
-        //            outer(0);
-        //        }
-        //    }
-        //
-        //    function decideTypeLevel() {
-        //        var f = 0,
-        //            c = 0,
-        //            f1 = 0,
-        //            c1 = 0,
-        //            type = '',
-        //            typeId = 0;
-        //        console.log(inputArr[1]['detail'] + '   34567')
-        //        !function getType() {
-        //            if (inputArr[1]['detail']) {
-        //                type = inputArr[1]['detail'];
-        //
-        //                sql = "select FireTypeId  from t_firetype where FireTypeName='"
-        //                    + type + "'";
-        //                db.query(sql, function (err, results) {
-        //                    if (err) {
-        //                        console.log(err.message);
-        //                        return
-        //                    }
-        //                    typeId = results[0]['FireTypeId'];
-        //                    c = inputArr[1]['truth0'];
-        //                    f = inputArr[1]['truth1'];
-        //                    f1 = f = (f * 1.0);
-        //                    c1 = c = (c * 0.9 * 1.0 * f);
-        //                    console.log(('type= ' + type + " f=" + f + " c=" + c + ' typeId:' + typeId))
-        //                    getLevel();
-        //                })
-        //            }
-        //
-        //        }();
-        //        function getLevel() {
-        //
-        //            db.query('call findMax()', function (err, results) {
-        //                if (err) {
-        //                    console.log(err.message);
-        //                    return
-        //                }
-        //                db.query("select * from t_conclusion ", function (err, results) {
-        //                    if (err) {
-        //                        console.log(err.message);
-        //                        return
-        //                    }
-        //                    var f2 = results[0]['f'],
-        //                        c2 = results[0]['c'],
-        //                        id = results[0]['conclusion']
-        //
-        //                    console.log("最终级别：LevelId=" + id);
-        //                    f = f1 * f2;
-        //                    c = c1 * c2;
-        //
-        //                    sql = "select * from t_dispatch " + "where TypeId = " + typeId
-        //                        + " AND LevelId = " + id + ";";
-        //
-        //                    db.query(sql, function (err, rs) {
-        //                        if (err) {
-        //                            console.log(err.message);
-        //                            return
-        //                        }
-        //                        console.log(sql)
-        //                        var conclu = rs[0]["DispatchId"],
-        //                            FireFighterNum = rs[0]["FireFighterNum"],
-        //                            Equipment = rs[0]["Equipment"],
-        //                            resultStr = '\n' + "$事件→[火灾类别:" + typeId + " ]+[火灾级别:"
-        //                                + id + " ] => $事件→(出动人数:" + FireFighterNum
-        //                                + ")+(设备数目:" + Equipment + ")（"
-        //                                + (1.0 * f) + "," + (0.9 * c)
-        //                                + "）";
-        //                        console.log(resultStr)
-        //                        res.status(200).send({returnState: 1, dispatch: resultStr, tipStr: tipStr});
-        //                        db.query("insert into t_conclusion values("
-        //                            + conclu + "," + f + "," + c + ")", function (err) {
-        //                            if (err) {
-        //                                console.log(err.message);
-        //                                return
-        //                            }
-        //                        });
-        //
-        //                    })
-        //                })
-        //
-        //            })
-        //        };
-        //    }
-        //
-
-
     }
 };
